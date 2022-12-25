c246850c69bebfad36538796d90bbe9d480979ab
poboxxdev2 <>
2022-12-25 21:41:59 +0600

    FileUpload

---
+++ HSUMHS.DAL/Repository/Repository.cs
@@ -0,0 +1,47 @@
+﻿using HSUMHS.DAL.Data;
+using HSUMHS.DAL.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSUMHS.DAL.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HSUMHS.DAL/Repository/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using HSUMHS.DAL.Data;
+using HSUMHS.DAL.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSUMHS.DAL.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+          
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
